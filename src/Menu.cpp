ad002a7caddf9a46f961cf5ebccdf12dab9fc222
Jihun Kim <appliedalpha@naver.com>
2024-06-03 22:20:00 +0900

    Menu Text의 얕은 참조 문제 해결

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -5,14 +5,17 @@ Menu::Menu(std::vector<std::string> vec)
     this->font = new sf::Font();
     this->font->loadFromFile("./resources/font/Arial.ttf");
 
-    this->menu.resize(vec.size(), new sf::Text());
+    this->menu.resize(vec.size());
 
     for (int i=0; i<this->menu.size(); i++) 
     {
+        this->menu[i] = new sf::Text();
+
         this->menu[i]->setFont(*(this->font));
         this->menu[i]->setFillColor(i == selectedIndex ? sf::Color::Cyan : sf::Color::White);
+        this->menu[i]->setCharacterSize(40.f);
         this->menu[i]->setString(vec[i]);
-        this->menu[i]->setPosition(CustomMath::GetCenterPos(CustomMath::CENTER, 360 + 70 * i, this->menu[i]->getLocalBounds().width));
+        this->menu[i]->setPosition(CustomMath::GetCenterPos(CustomMath::CENTER, 360 + 75 * i, this->menu[i]->getLocalBounds().width));
     }
 
     selectedIndex = 0;
