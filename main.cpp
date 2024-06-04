0ebf001a35767519b3eb4588af4e81028830d856
Jihun Kim <appliedalpha@naver.com>
2024-06-04 15:08:35 +0900

    메인 주석 제거

--- main.cpp
+++ main.cpp
@@ -1,61 +1,11 @@
 #include "src/GameManager.h"
 
-// ÇÁ·Î±×·¥ÀÇ ÁøÀÔÁ¡
+// ï¿½ï¿½ï¿½Î±×·ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 int main()
 {
-    // °ÔÀÓÀ» °ü¸®ÇÏ´Â Å¬·¡½º¸¦ ¸¸µé¾î ½ÇÇà½ÃÅ´
+    // ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½Ï´ï¿½ Å¬ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½Å´
     GameManager game;
     game.Run();
 
     return 0;
 }
-
-
-/*
-#include <SFML/Graphics.hpp>
-#include "Menu.h"
-
-int main() {
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "Brick Pong");
-    Menu menu(window.getSize().x, window.getSize().y);
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-            if (event.type == sf::Event::KeyPressed) {
-                switch (event.key.code) {
-                case sf::Keyboard::Up:
-                    menu.moveUp();
-                    break;
-                case sf::Keyboard::Down:
-                    menu.moveDown();
-                    break;
-                case sf::Keyboard::Return:
-                    switch (menu.getSelectedIndex()) {
-                    case 0:
-                        std::cout << "Game Start pressed" << std::endl;
-                        // °ÔÀÓ ½ÃÀÛ ·ÎÁ÷ Ãß°¡
-                        break;
-                    case 1:
-                        std::cout << "Setting pressed" << std::endl;
-                        // ¼³Á¤ ¸Þ´º ·ÎÁ÷ Ãß°¡
-                        break;
-                    case 2:
-                        window.close();
-                        break;
-                    }
-                    break;
-                }
-            }
-        }
-
-        window.clear();
-        menu.draw(window);
-        window.display();
-    }
-
-    return 0;
-}
-*/
